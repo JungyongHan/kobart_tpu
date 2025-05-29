981890588e0d91b60da45d4c62d20bd86027bf42
Jungyong Han <hjyjj554@koroad.or.kr>
2025-05-29 16:11:36 +0900

    chore: fix trailing whitespace in run_train.

--- run_train.sh
+++ run_train.sh
@@ -9,4 +9,4 @@ PJRT_DEVICE=TPU | ~/venv/bin/python train.py --gradient_clip_val 1.0 \
                 --num_workers 4 \
                 --precision bf16-mixed \
                 --train_file "../summary/dataset/news_data.csv" \
-                --test_file "../summary/dataset/2022_2025.csv"
+                --test_file "../summary/dataset/2022_2025.csv" 
