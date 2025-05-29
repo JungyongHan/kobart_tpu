181bd6c4183050cb4a68276c45b6b25c002700e3
Jungyong Han <hjyjj554@koroad.or.kr>
2025-05-29 16:54:43 +0900

    s

--- run_train.sh
+++ run_train.sh
@@ -6,6 +6,6 @@ PJRT_DEVICE=TPU | ~/venv/bin/python train.py --gradient_clip_val 1.0 \
                 --accelerator tpu \
                 --batch_size 32 \
                 --num_workers 4 \
-                --precision bf16-mixed \
+                --precision bf16-true \
                 --train_file "./data/train.csv" \
                 --test_file "./data/test.csv" 
