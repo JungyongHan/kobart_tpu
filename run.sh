2ff939f84fb31e76ebf888b5a4ed025bdc772c45
Jungyong Han <hjyjj554@koroad.or.kr>
2025-05-29 13:54:48 +0900

    first commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+python train.py --gradient_clip_val 1.0 \
+                --max_epochs 100 \
+                --checkpoint checkpoint \
+                --accelerator tpu \
+                --num_gpus 16 \
+                --batch_size 8 \
+                --num_workers 4 \
+		--train_file "./data/train.csv" \
+		--test_file "./data/test.csv"
